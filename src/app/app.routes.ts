9144ae3a4ad02cbb50995064c49e7a9ab44e45dd
lowtab777 <chikiker07@gmail.com>
2025-06-11 20:57:26 +0300

    created 3 additionals tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { NewsPageComponent } from './components/news-page/news-page.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { TeamPageComponent } from './components/team-page/team-page.component';
+import { LicensesPageComponent } from './components/licenses-page/licenses-page.component';
+import { ProductsPageComponent } from './components/products-page/products-page.component';
 
 export const routes: Routes = [
     {path:'news', component: NewsPageComponent},
     {path:'contacts', component: ContactsComponent},
     {path:'about-us', component: AboutUsComponent},
     {path:'main-page', component: MainPageComponent},
+    {path:'team', component: TeamPageComponent},
+    {path:'licenses', component: LicensesPageComponent},
+    {path:'products', component: ProductsPageComponent},
     {path:'**', component: MainPageComponent},
 ];
---
+++ src/app/components/licenses-page/licenses-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-licenses-page',
+  imports: [],
+  templateUrl: './licenses-page.component.html',
+  styleUrl: './licenses-page.component.scss'
+})
+export class LicensesPageComponent {
+
+}
