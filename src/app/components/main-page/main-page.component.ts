3e53d83f6a03f63c63c78fe01d845385138adff7
lowtab777 <chikiker07@gmail.com>
2026-04-23 19:51:42 +0300

    latest stable

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-import { PageHeadlineComponent } from '../../shared/ui/page-headline/page-headline.component';
 import { InfoBlockComponent } from '../../shared/ui/info-block/info-block.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [MatButtonModule, RouterModule, PageHeadlineComponent, InfoBlockComponent],
+  imports: [MatButtonModule, RouterModule, InfoBlockComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
